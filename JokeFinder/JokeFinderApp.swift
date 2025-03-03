d91fcb209c08e8e15ef871e3ed72756a05e350c8
Jayden Kim <hkim@lcs.on.ca>
2025-03-03 14:25:29 -0500

    Initial Commit

---
+++ JokeFinder/JokeFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokeFinderApp.swift
+//  JokeFinder
+//
+//  Created by kim heejun on 3/3/25.
+//
+
+import SwiftUI
+
+@main
+struct JokeFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
